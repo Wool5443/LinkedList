d9368c87046af76c270a11047c12a1ce14a56ce6
Wool5443 <mihsolodilov2015@gmail.com>
2023-11-13 01:24:19 +0300

    mt

--- src/PrettyDumpList.cpp
+++ src/PrettyDumpList.cpp
@@ -14,6 +14,14 @@
 static const size_t MAX_LEN_PATH    = 128;
 static const size_t MAX_COMMAND_LEN = 512;
 
+#define PRINT_LOG(...)                                                          \
+do                                                                              \
+{                                                                               \
+    fprintf(outTextFile, __VA_ARGS__);                                          \
+    if (HTML_FILE)                                                              \
+        fprintf(HTML_FILE, __VA_ARGS__);                                        \
+} while (0)             
+
 ErrorCode StartHtmlLogging()
 {
     HTML_FILE = fopen(DEFAULT_HTML_LOG, "w");
@@ -39,7 +47,14 @@ ErrorCode _dumpList(LinkedList* list, ErrorCode error, SourceCodePosition* calle
     char outGraphPath[MAX_LEN_PATH] = "";
     sprintf(outGraphPath, "log/dot/iter%zu.dot", DUMP_ITER);
 
+    if (HTML_FILE)
+        fprintf(HTML_FILE, "<h1>iter%zu</h1>\n<pre>\n", DUMP_ITER);
+
     RETURN_ERROR(_dumpListText(list, error, caller, outTextPath));
+    
+    if (HTML_FILE)
+        fprintf(HTML_FILE, "</pre>\n");
+
     RETURN_ERROR(DumpListGraph(list, outGraphPath));
 
     char outImgPath[MAX_LEN_PATH] = "";
@@ -51,22 +66,7 @@ ErrorCode _dumpList(LinkedList* list, ErrorCode error, SourceCodePosition* calle
     system(command);
 
     if (HTML_FILE)
-    {
-        fprintf(HTML_FILE, "<h1>iter%zu</h1>\n", DUMP_ITER);
-        
-        size_t txtSize = GetFileSize(outTextPath);
-        char* txtData = (char*)calloc(txtSize + 1, 1);
-
-        FILE* txt = fopen(outTextPath, "r");
-        MyAssertSoft(txt, ERROR_BAD_FILE);
-        fread(txtData, 1, txtSize, txt);
-        fclose(txt);
-
-        fprintf(HTML_FILE, "<pre>\n%s</pre>\n", txtData);
-        free(txtData);
-
         fprintf(HTML_FILE, "<img src = \"%s\"/>\n", outImgPath);
-    }
     DUMP_ITER++;
     return EVERYTHING_FINE;
 }
@@ -80,65 +80,65 @@ ErrorCode _dumpListText(LinkedList* list, ErrorCode error, SourceCodePosition* c
     FILE* outTextFile = fopen(outTextPath, "w");
     MyAssertSoft(outTextFile, ERROR_BAD_FILE);
 
-    fprintf(outTextFile, "List[%p] called from %s(%zu) %s()\n", list, caller->fileName,
+    PRINT_LOG("List[%p] called from %s(%zu) %s()\n", list, caller->fileName,
                                                                        caller->line,
                                                                        caller->name);
-    fprintf(outTextFile, "List condition - %s[%d]\n", ERROR_CODE_NAMES[error], error);
+    PRINT_LOG("List condition - %s[%d]\n", ERROR_CODE_NAMES[error], error);
 
-    fprintf(outTextFile, "{\n");
-    fprintf(outTextFile, "%3s length = %zu\n", "",    list->length);
-    fprintf(outTextFile, "%3s capacity = %zu\n", "",  list->capacity);
-    fprintf(outTextFile, "%3s head = %zu\n", "",     *list->head);
-    fprintf(outTextFile, "%3s tail = %zu\n", "",     *list->tail);
-    fprintf(outTextFile, "%3s free head = %zu\n", "", list->freeHead);
-    fprintf(outTextFile, "%3s list:\n", "");
+    PRINT_LOG("{\n");
+    PRINT_LOG("%3s length = %zu\n", "",    list->length);
+    PRINT_LOG("%3s capacity = %zu\n", "",  list->capacity);
+    PRINT_LOG("%3s head = %zu\n", "",     *list->head);
+    PRINT_LOG("%3s tail = %zu\n", "",     *list->tail);
+    PRINT_LOG("%3s free head = %zu\n", "", list->freeHead);
+    PRINT_LOG("%3s list:\n", "");
     {
         size_t curEl    = *list->head;
         size_t orderNum = 1;
         while (curEl != 0 && orderNum <= list->length * 2)
         {
-            fprintf(outTextFile, "%4s", "");
+            PRINT_LOG("%4s", "");
             if (list->data[curEl] != LIST_POISON)
-                fprintf(outTextFile, "*[%zu] = " LIST_EL_SPECIFIER "\n", orderNum, list->data[curEl]);
+                PRINT_LOG("*[%zu] = " LIST_EL_SPECIFIER "\n", orderNum, list->data[curEl]);
             else
-                fprintf(outTextFile, " [%zu] = POISON\n", orderNum);
+                PRINT_LOG(" [%zu] = POISON\n", orderNum);
 
             curEl = list->next[curEl];
             orderNum++;
         }
     }
 
-    fprintf(outTextFile, "\n%3s data[%p]\n", "", list->data);
+    PRINT_LOG("\n%3s data[%p]\n", "", list->data);
     for (size_t i = 0; i < list->capacity; i++)
     {
-        fprintf(outTextFile, "%4s", "");
+        PRINT_LOG("%4s", "");
         if (list->data[i] != LIST_POISON)
-            fprintf(outTextFile, "*[%zu] = " LIST_EL_SPECIFIER "\n", i, list->data[i]);
+            PRINT_LOG("*[%zu] = " LIST_EL_SPECIFIER "\n", i, list->data[i]);
         else
-            fprintf(outTextFile, " [%zu] = POISON\n", i);
+            PRINT_LOG(" [%zu] = POISON\n", i);
     }
 
-    fprintf(outTextFile, "\n%3s prev[%p]\n", "", list->prev);
+    PRINT_LOG("\n%3s prev[%p]\n", "", list->prev);
     for (size_t i = 0; i < list->capacity; i++)
     {
-        fprintf(outTextFile, "%4s", "");
+        PRINT_LOG("%4s", "");
         if (list->prev[i] != FREE_ELEM)
-            fprintf(outTextFile, "*[%zu] = %zu\n", i, list->prev[i]);
+            PRINT_LOG("*[%zu] = %zu\n", i, list->prev[i]);
         else
-            fprintf(outTextFile, " [%zu] = FREE\n", i);
+            PRINT_LOG(" [%zu] = FREE\n", i);
     }
 
-    fprintf(outTextFile, "\n%3s next[%p]\n", "", list->next);
+    PRINT_LOG("\n%3s next[%p]\n", "", list->next);
     for (size_t i = 0; i < list->capacity; i++)
     {
-        fprintf(outTextFile, "%4s", "");
+        PRINT_LOG("%4s", "");
         if (list->next[i] != FREE_ELEM)
-            fprintf(outTextFile, "*[%zu] = %zu\n", i, list->next[i]);
+            PRINT_LOG("*[%zu] = %zu\n", i, list->next[i]);
         else
-            fprintf(outTextFile, " [%zu] = BAD\n", i);
+            PRINT_LOG(" [%zu] = BAD\n", i);
     }
         
-    fprintf(outTextFile, "}\n");
+    PRINT_LOG("}\n");
 
     fclose(outTextFile);
 
